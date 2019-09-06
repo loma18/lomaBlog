78b97b4a80d465793426f7a486cc1919145e8a8e
Loma <909622852@163.com>
2019-09-06 17:30:53 +0800

    更新数据库密码

--- sqlConnect/index.js
+++ sqlConnect/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 let pool = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
-    password: "",
+    password: "123456",
     database: "lomaBlog",
     port: 3306,
     connectionLimit: 10
